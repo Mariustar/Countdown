21b35e7a999c3162873b9c794e769bc4f669c1ce
Marius Aruxandei <mariushow@gmail.com>
2022-05-12 21:57:16 +0300

    this is not bad after all

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ class Timer {
   }
 
   start() {
-    console.log("Time to start the timer");
+    console.log(this);
 
   }
+
+  // importantMethodToCall() {
+  //   console.log("Important thing was called");
+  // }
+
 }
 
 const durationInput = document.querySelector("#duration");
@@ -18,3 +23,25 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
+
+// console.log(this);
+// const printThis = () => {
+//   console.log(this);
+// }
+
+
+const colors= {
+  printColor() {
+    console.log(this);
+  },
+  "Marius": "Ionut"
+}
+
+const randomObject = {
+  a : 1
+}
+
+randomObject.printColor = colors.printColor;
+
+colors.printColor();
+randomObject.printColor();
