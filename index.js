4c6886de6aab9586776cf61964ed6c4e62e6c20d
Marius Aruxandei <mariushow@gmail.com>
2022-05-13 14:05:10 +0300

    basic linting config and fixes

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-
     startButton.addEventListener("click", this.start);
     pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
     this.tick();
-    this.intervalID = setInterval(this.tick, 1000)
-  }
+    this.intervalID = setInterval(this.tick, 1000);
+  };
 
   pause = () => {
     clearInterval(this.intervalID);
-  }
+  };
 
   tick = () => {
     const timeRemaning = parseFloat(this.durationInput.value);
     this.durationInput.value = timeRemaning - 1;
-  }
+  };
 }
 
 const durationInput = document.querySelector("#duration");
@@ -28,4 +27,3 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-
