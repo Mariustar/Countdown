d0d7bae4102fd60caf688db88f4015ecf5c29e74
Marius Aruxandei <mariushow@gmail.com>
2022-05-13 16:03:45 +0300

    implement callbacks to announce outside the class

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
     startButton.addEventListener("click", this.start);
     pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.intervalID = setInterval(this.tick, 1000);
   };
@@ -17,8 +25,13 @@ class Timer {
   };
 
   tick = () => {
-    if (this.timeRemaining <= 0) {
+    if (this.onTick) {
+      this.onTick();
+    }
+    if (this.timeRemaining <= 1) {
       this.pause();
+      this.onComplete();
+      this.timeRemaining = 0;
     } else {
       this.timeRemaining -= 1;
     }
@@ -37,4 +50,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer Started!");
+  },
+  onTick() {
+    console.log("1 Second has elapsed!");
+  },
+  onComplete() {
+    console.log("Completed!");
+  },
+});
