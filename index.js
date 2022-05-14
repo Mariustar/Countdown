370a19b2a89909cd1f1a145025905569f19b256f
Marius Aruxandei <mariushow@gmail.com>
2022-05-15 00:38:46 +0300

    finish version 1

--- index.js
+++ index.js
@@ -12,19 +12,29 @@ const perimeter = Math.round(circle.getAttribute("r") * 2 * Math.PI);
 circle.setAttribute("stroke-dasharray", perimeter);
 
 let duration = 0;
+let timeElapsed = 0;
+let counter = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, resetButton, {
   onStart(totalDuration) {
     duration = totalDuration;
+    if (counter >= 1) {
+      duration = totalDuration + timeElapsed;
+    }
   },
   onTick(timeRemaining) {
     const strokeValue = (perimeter * timeRemaining) / duration - perimeter;
     circle.setAttribute("stroke-dashoffset", strokeValue);
+    timeElapsed = duration - timeRemaining;
   },
   onComplete() {
-    circle.setAttribute("stroke-dashoffset", perimeter);
+    circle.setAttribute("stroke-dashoffset", 0);
+  },
+  onPause() {
+    counter += 1;
   },
   onReset() {
+    circle.setAttribute("stroke-dashoffset", 0);
     durationInput.value = 0;
   },
 });
